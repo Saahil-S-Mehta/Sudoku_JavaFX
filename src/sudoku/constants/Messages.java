7af5bf979559047874082a5cdc62064ec8fd0132
Saahil-S-Mehta <saahils.mehta@gmail.com>
2022-09-06 01:52:23 +0530

    Initial

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,7 @@
+package sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "Congratulations, you have won.";
+    public static final String ERROR = "An error has occurred.";
+
+}
---
+++ src/sudoku/userinterface/UserInterfaceImpl.java
@@ -0,0 +1,271 @@
+package sudoku.userinterface;
+
+import sudoku.constants.GameState;
+import sudoku.problemdomain.Coordinates;
+import sudoku.problemdomain.SudokuGame;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+
+
+public class UserInterfaceImpl implements IUserInterfaceContract.View,
+        EventHandler<KeyEvent> {
+    private final Stage stage;
+    private final Group root;
+
+    private HashMap<Coordinates, SudokuTextField> textFieldCoordinates;
+
+    private IUserInterfaceContract.EventListener listener;
+
+    private static final double WINDOW_Y = 732;
+    private static final double WINDOW_X = 668;
+
+    private static final double BOARD_PADDING = 50;
+
+    private static final double BOARD_X_AND_Y = 576;
+    private static final Color WINDOW_BACKGROUND_COLOR = Color.rgb(255, 0, 0);
+    private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(255, 242, 241);
+    private static final String SUDOKU = "SUDOKU";
+    private static final String NAME = "By: Sanga Bhavesh(1RV19CS136)\n      Saahil Mehta(1RV19CS133)";
+
+    public UserInterfaceImpl(Stage stage) {
+        this.stage = stage;
+        this.root = new Group();
+        this.textFieldCoordinates = new HashMap<>();
+        initializeUserInterface();
+    }
+
+
+    @Override
+    public void setListener(IUserInterfaceContract.EventListener listener) {
+        this.listener = listener;
+    }
+
+    public void initializeUserInterface() {
+        drawBackground(root);
+        drawTitle(root);
+        drawSudokuBoard(root);
+        drawTextFields(root);
+        drawGridLines(root);
+        stage.show();
+    }
+
+
+    private void drawTextFields(Group root) {
+        final int xOrigin = 50;
+        final int yOrigin = 50;
+        final int xAndYDelta = 64;
+
+
+        for (int xIndex = 0; xIndex < 9; xIndex++) {
+            for (int yIndex = 0; yIndex < 9; yIndex++) {
+                int x = xOrigin + xIndex * xAndYDelta;
+                int y = yOrigin + yIndex * xAndYDelta;
+                SudokuTextField tile = new SudokuTextField(xIndex, yIndex);
+
+                styleSudokuTile(tile, x, y);
+
+                tile.setOnKeyPressed(this);
+
+                textFieldCoordinates.put(new Coordinates(xIndex, yIndex), tile);
+
+                root.getChildren().add(tile);
+            }
+        }
+    }
+
+    private void styleSudokuTile(SudokuTextField tile, double x, double y) {
+        Font numberFont = new Font(32);
+        tile.setFont(numberFont);
+        tile.setAlignment(Pos.CENTER);
+
+        tile.setLayoutX(x);
+        tile.setLayoutY(y);
+        tile.setPrefHeight(64);
+        tile.setPrefWidth(64);
+
+        tile.setBackground(Background.EMPTY);
+    }
+
+    private void drawGridLines(Group root) {
+        //draw vertical lines starting at 114x and 114y:
+        int xAndY = 114;
+        int index = 0;
+        while (index < 8) {
+            int thickness;
+            if (index == 2 || index == 5) {
+                thickness = 3;
+            } else {
+                thickness = 2;
+            }
+
+            Rectangle verticalLine = getLine(
+                    xAndY + 64 * index,
+                    BOARD_PADDING,
+                    BOARD_X_AND_Y,
+                    thickness
+            );
+
+            Rectangle horizontalLine = getLine(
+                    BOARD_PADDING,
+                    xAndY + 64 * index,
+                    thickness,
+                    BOARD_X_AND_Y
+            );
+
+            root.getChildren().addAll(
+                    verticalLine,
+                    horizontalLine
+            );
+
+            index++;
+        }
+    }
+
+    public Rectangle getLine(double x, double y, double height, double width){
+        Rectangle line = new Rectangle();
+
+        line.setX(x);
+        line.setY(y);
+
+        line.setHeight(height);
+        line.setWidth(width);
+
+        line.setFill(Color.BLACK);
+        return line;
+
+    }
+
+    private void drawBackground(Group root) {
+        Scene scene = new Scene(root, WINDOW_X, WINDOW_Y);
+        scene.setFill(WINDOW_BACKGROUND_COLOR);
+        stage.setScene(scene);
+    }
+
+
+    private void drawSudokuBoard(Group root) {
+        Rectangle boardBackground = new Rectangle();
+        boardBackground.setX(BOARD_PADDING);
+        boardBackground.setY(BOARD_PADDING);
+        boardBackground.setWidth(BOARD_X_AND_Y);
+        boardBackground.setHeight(BOARD_X_AND_Y);
+        boardBackground.setFill(BOARD_BACKGROUND_COLOR);
+        root.getChildren().add(boardBackground);
+    }
+
+    private void drawTitle(Group root) {
+        Text title = new Text(290, 35, SUDOKU);
+        title.setFill(Color.BLACK);
+        Font titleFont = new Font(30);
+        title.setFont(titleFont);
+        root.getChildren().add(title);
+        Text name = new Text(220, 690, NAME);
+        name.setFill(Color.BLACK);
+        Font nameFont = new Font(18);
+        name.setFont(nameFont);
+        root.getChildren().add(name);
+    }
+
+    @Override
+    public void updateSquare(int x, int y, int input) {
+        SudokuTextField tile = textFieldCoordinates.get(new Coordinates(x, y));
+        String value = Integer.toString(
+                input
+        );
+
+        if (value.equals("0")) value = "";
+
+        tile.textProperty().setValue(value);
+    }
+
+    @Override
+    public void updateBoard(SudokuGame game) {
+        for (int xIndex = 0; xIndex < 9; xIndex++) {
+            for (int yIndex = 0; yIndex < 9; yIndex++) {
+                TextField tile = textFieldCoordinates.get(new Coordinates(xIndex, yIndex));
+
+                String value = Integer.toString(
+                        game.getCopyOfGridState()[xIndex][yIndex]
+                );
+
+                if (value.equals("0")) value = "";
+                tile.setText(
+                        value
+                );
+
+                if (game.getGameState() == GameState.NEW){
+                    if (value.equals("")) {
+                        tile.setStyle("-fx-opacity: 1;");
+                        tile.setDisable(false);
+                    } else {
+                        tile.setStyle("-fx-opacity: 0.8;");
+                        tile.setDisable(true);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void showDialog(String message) {
+        Alert dialog = new Alert(Alert.AlertType.CONFIRMATION, message, ButtonType.OK);
+        dialog.showAndWait();
+
+        if (dialog.getResult() == ButtonType.OK) listener.onDialogClick();
+    }
+
+    @Override
+    public void showError(String message) {
+        Alert dialog = new Alert(Alert.AlertType.ERROR, message, ButtonType.OK);
+        dialog.showAndWait();
+    }
+
+
+    @Override
+    public void handle(KeyEvent event) {
+        if (event.getEventType() == KeyEvent.KEY_PRESSED) {
+            if (event.getText().equals("0")
+                    || event.getText().equals("1")
+                    || event.getText().equals("2")
+                    || event.getText().equals("3")
+                    || event.getText().equals("4")
+                    || event.getText().equals("5")
+                    || event.getText().equals("6")
+                    || event.getText().equals("7")
+                    || event.getText().equals("8")
+                    || event.getText().equals("9")
+            ) {
+                int value = Integer.parseInt(event.getText());
+                handleInput(value, event.getSource());
+            } else if (event.getCode() == KeyCode.BACK_SPACE) {
+                handleInput(0, event.getSource());
+            } else {
+                ((TextField)event.getSource()).setText("");
+            }
+        }
+
+        event.consume();
+    }
+
+    private void handleInput(int value, Object source) {
+        listener.onSudokuInput(
+                ((SudokuTextField) source).getX(),
+                ((SudokuTextField) source).getY(),
+                value
+        );
+    }
+}
